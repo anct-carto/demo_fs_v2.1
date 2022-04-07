bc27a60b3ff24fb2f02b9a9b8ecd4aa5bc6ccc09
HassChg <52163102+HassChg@users.noreply.github.com>
2022-04-07 12:05:57 +0200

    ajout intro fiche pdf + css

--- src/app.js
+++ src/app.js
@@ -348,7 +348,7 @@ let searchGroupComponent = {
 
 
 
-// ****************************************************************************
+// FICHE PDF ****************************************************************************
 
 window.jsPDF = window.jspdf.jsPDF
 
@@ -422,24 +422,25 @@ let pdfComponent = {
                 }
             });
         }, 500);
-
-
     },
     template:`
-        <div class="container-sm" id="fiche-pdf" style="font-family:'Marianne-Regular'">
-            <div>
-                <img src="img/logo_anct.png" style="height:110px;margin-bottom:40px;margin-left:-30px">
-                <img src="img/logo_FranceServices_sans-marianne-01.jpg" style="height:65px;margin-bottom:20px;margin-left:280px">
-                <!--<img src="img/logo_FranceServices-01.png" style="height:150px;margin-bottom:40px;">-->
-            </div>
-            <div class="row" style="background:rgb(254,254,254);font-size:1.1em">
-                <div class="col-8 p-0">
+        <div class="container-sm" id="fiche-pdf">
+        <div class="row">
+                <div class="header-pdf-logos">
+                    <img src="img/logo_rf.jpg" class="logo-rf">
+                    <img src="img/logo_FranceServices_sans-marianne-01.jpg" class="logo-fs">
+                </div>
+                <div class="col-11 p-0">
                     <span style="font-size:.8em">Fiche d'information France services - données extraites le {{ date }}</span>
                     <h2 style='font-weight:bolder'><b>{{ fs.lib_fs }}</b></h2><br>
                     <div class = "intro">
                         <p v-if="fs.itinerance=='oui'">
-                            <!--<i class="las la-exclamation-circle"></i> -->
                             <span>Attention : cette France services est en itinérance</span>
+                        </p>
+                        <p>
+                            Immatriculation de véhicules, RSA, impôt, permis de conduire, accès aux services en ligne... Vous avez besoin d’aide pour vos démarches administratives ? Quel que soit l’endroit où vous vivez, en ville ou à la campagne, France services est un guichet unique qui donne accès dans un seul et même lieu aux principaux organismes de services publics : le ministère de l'Intérieur, le ministère de la Justice, les Finances publiques, Pôle emploi, l'Assurance retraite, l'Assurance maladie, la CAF, la MSA et la Poste.</p>
+                        <p>
+                            Retrouvez la France services la plus proche de chez vous sur <a href="france-services.gouv.fr" target="_blank">france-services.gouv.fr</a> 
                         </p>
                         <div class="row">
                             <div class="col-6">
@@ -493,13 +494,12 @@ let pdfComponent = {
                                     <span v-if = "fs.mail"><b>Courriel : </b><a v-bind:href = "'mailto:' + fs.mail" target = "_blank">{{ fs.mail }}</a></span>
                                 </div><br>
                             </div>
-                            <div class="col-5">
-                                <div id="map-pdf" style="height:300px"></div>
+                            <div class="col-6">
+                                <div id="map-pdf"></div>
                             </div>
                         </div>
                     </div><br>
                     <div class="corps">
-
                         <div v-if="fs.commentaire_horaires">
                             <!--<i class = "las la-info-circle"></i>-->
                             <h5><b>Commentaire(s)</b></h5>
